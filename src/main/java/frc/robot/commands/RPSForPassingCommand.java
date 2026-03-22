ca5f8b446cb7ff32c20812e1b68a93bb9de04ea9
becauseYES <nreddyeshaan@gmail.com>
2026-03-21 21:46:03 -0500

    passing rps command

---
+++ src/main/java/frc/robot/commands/RPSForPassingCommand.java
@@ -0,0 +1,61 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import static edu.wpi.first.units.Units.Meters;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.GameConstants;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.KickerSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class RPSForPassingCommand extends Command{
+        private final ShooterSubsystem shooter;
+    private final KickerSubsystem kicker;
+    private final IndexerSubsystem indexer;
+
+    private final Supplier<Pose2d> currentPoseSupplier;
+
+    public RPSForPassingCommand(
+            ShooterSubsystem shooter,
+            KickerSubsystem kicker,
+            IndexerSubsystem indexer,
+            Supplier<Pose2d> currentPoseSupplier) {
+        this.shooter = shooter;
+        this.kicker = kicker;
+        this.indexer = indexer;
+        this.currentPoseSupplier = currentPoseSupplier;
+
+        addRequirements(shooter, kicker, indexer);
+    }
+
+    @Override
+    public void execute() {
+        Pose2d currentPose = currentPoseSupplier.get();
+        Distance targetX = (DriverStation.getAlliance().get() == Alliance.Blue)
+                ? Meters.of(2.306)
+                : Meters.of(14.207);
+        Translation2d hubLocation = (DriverStation.getAlliance().get() == Alliance.Blue)
+                ? new Translation2d(2.03, currentPose.getTranslation().getY())
+                : new Translation2d(2.03, currentPose.getTranslation().getY());
+
+        Distance shotGroundDistance = Meters
+                .of(Math.abs(currentPose.getTranslation().getX() - targetX.in(Meters)));
+
+        shooter.shoot(shooter.calculateRPS(shotGroundDistance));
+
+        indexer.enable();
+        kicker.kick();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
